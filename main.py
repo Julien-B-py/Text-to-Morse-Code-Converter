6569ca59a52a8847102b9088f351a3cb5addbab7
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-11-18 15:50:00 +0100

    v0.1
    Minor comments changes.

--- main.py
+++ main.py
@@ -11,17 +11,20 @@ def to_morse_code(base_message: str) -> str:
     Returns:
         encoded_message (str): Converted string
     """
-    # Loop through all characters in message and check if it is a "space".
-    # If the character is not a "space" then pick the corresponding value from MORSE_TABLE dict.
-    # Else just add a space.
-    # Use join to return a string containing all encrypted characters.
-    encoded_message = [f'{MORSE_TABLE.get(character, "*")} ' if character != ' ' else ' ' for character in base_message]
-    if '* ' in encoded_message:
+    # Loop through all characters in base_message and check if it is a "space".
+    # If the character is not a "space" then pick the corresponding value from MORSE_TABLE dict and add it to
+    # encoded_message.
+    # If no match found for the current character in MORSE_TABLE dict -> add "*" character to encoded_message.
+    # Else just add a space to encoded_message.
+    encoded_message = [f"{MORSE_TABLE.get(character, '*')} " if character != " " else " " for character in base_message]
+    # If a character couldn't be encrypted display a message
+    if "* " in encoded_message:
         print("Some characters couldn't be encrypted you will see '*' instead.")
-    return ''.join(encoded_message)
+    # Use join to return a string containing all encrypted characters.
+    return "".join(encoded_message)
 
 
-if __name__ == '__main__':
-    message_to_encrypt = input('Text to convert to Morse Code: ').upper()
+if __name__ == "__main__":
+    message_to_encrypt = input("Text to convert to Morse Code: ").upper()
     result = to_morse_code(message_to_encrypt)
-    print(f'Result: {result}')
+    print(f"Result: {result}")
